02db40fcffa079022e2def74b31e24900b3a7aa9
肖云朋 <yunpengxiao@creditease.cn>
2016-11-16 15:51:47 +0800

    ADD https and http support

--- new_proxy_http.sh
+++ new_proxy_http.sh
@@ -29,39 +29,11 @@ done
 r_code=$(echo -e $r_code)
 i_code=$(echo -e $i_code)
 
-cat > ${o_domain}.conf<<EOF
+cat > ${o_domain}_80.conf<<EOF
 
 server {
     listen 80;
     server_name ${n_domain};
-
-    location / {
-        proxy_pass http://${o_domain};
-        proxy_set_header Host "${o_domain}";
-        proxy_set_header X-Real-IP \$remote_addr;
-        # 为替换，需禁用后端的压缩
-        proxy_set_header Accept-Encoding  "";
-        # 替换跳转
-        proxy_redirect default;
-        proxy_redirect https://${o_domain} https://${n_domain};
-        
-        sub_filter_once off;
-        # 替换逻辑
-$r_code
-
-        # 禁用逻辑，只是把相关字串去掉，不能删除整行
-$i_code
-    }
-}
-
-
-server {
-    listen 443;
-    server_name ${n_domain};
-
-    ssl on;
-    ssl_certificate     ssl/${n_domain}.crt;
-    ssl_certificate_key ssl/${n_domain}.key;
 
     location / {
         proxy_pass https://${o_domain};
@@ -69,20 +41,41 @@ server {
         proxy_set_header X-Real-IP \$remote_addr;
         # 为替换，需禁用后端的压缩
         proxy_set_header Accept-Encoding  "";
+        proxy_hide_header "Content-Security-Policy";
+        proxy_hide_header "Content-Security-Policy-Report-Only";
+        proxy_hide_header "X-Frame-Options";
         # 替换跳转
         proxy_redirect default;
-        proxy_redirect https://${o_domain} https://${n_domain};
+        proxy_redirect http://${o_domain} http://${n_domain};
+        proxy_redirect https://${o_domain} http://${n_domain};
 
-
+        header_filter_by_lua '
+            local cookies= ngx.header.set_cookie
+            if not cookies then
+                return
+            end
+            if type(cookies) ~= "table" then
+                cookies = {cookies}
+            end
+            
+            local newcookies = {}
+            for i, val in ipairs(cookies) do
+                local newval = string.gsub(val, " secure;", "")
+                table.insert(newcookies, newval)
+            end
+            ngx.header.set_cookie = newcookies;
+        ';
+        
+        sub_filter_types text/css text/javascript;
         sub_filter_once off;
         # 替换逻辑
         sub_filter '${o_domain}' '${n_domain}';
+        sub_filter 'https://${o_domain}' 'http://${n_domain}';
 $r_code
 
         # 禁用逻辑，只是把相关字串去掉，不能删除整行
 $i_code
     }
 }
-
 
 EOF
